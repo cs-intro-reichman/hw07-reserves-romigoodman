f1c20846c1995ee6ab41a8b220c703bb41a42879
romigoodman <romi.goodman@post.runi.ac.il>
2024-03-10 09:02:41 +0100

    trying

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -6,7 +6,7 @@ public class HashTagTokenizer {
 
 		String hashTag = args[0];
 		String []dictionary = readDictionary("dictionary.txt");
-		breakHashTag(hashTag, dictionary);
+		breakHashTag(hashTag.toLowerCase(), dictionary); // all be the same
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -14,27 +14,68 @@ public class HashTagTokenizer {
 
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i=0; i<dictionary.length; i++)
+		{
+			{
+			dictionary[i]=in.readLine(); 
+			}
+
+		}
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i=0; i<dictionary.length; i++) //(String WordInDictionary : dictionary) 
+		{
+			String wordInDictionary = dictionary[i];
+			if ( wordInDictionary.equals(dictionary))
+			{
+				return true;
+			}
+        }
+        
+        return false;
+		
+		// String myInput = word;
+		// boolean isPresent = false;
+
+		// //
+		// {
+		// if (myInput==dictionary[i])
+		// 	{
+		// 		isPresent=true;
+		// 	}
+		// else 
+		// 	{
+		// 	isPresent=false;
+		// 	}
+		// }
+		// return isPresent;
 	}
-
-	public static void breakHashTag(String hashtag, String[] dictionary) {
-
+	public static void breakHashTag(String hashtag, String[] dictionary) 
+	{
 		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
+        if (hashtag.isEmpty()) 
+		{
             return;
         }
- 
-        int N = hashtag.length();
 
-        for (int i = 1; i <= N; i++) {
-		
-        }
+		for (int i = 1; i <= hashtag.length(); i++) //starts at 1 because we want to consider prefixes of the hashtag string starting from a single character and gradually increasing in length
+		{
+			String prefix = hashtag.substring(0, i);
+			if (existInDictionary(prefix, dictionary)) 
+			{
+			System.out.println(prefix);
+			String remaining = hashtag.substring(i);
+			
+			if (!remaining.isEmpty()) 
+			{
+				breakHashTag(remaining, dictionary);
+			}
+			
+        	}
+		}	 	
     }
 
-}
+	
